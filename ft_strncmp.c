f5d1b0c9c0d5e8984ddee23b09ed0ccf7455000c
Dio <dionisia.martin.silva@gmail.com>
2023-05-07 21:48:45 +0200

    07/05/2023 21:48

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,24 +3,27 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	unsigned int 	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i <= n)
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (-1);
+		printf("%d \n", s1[i]);
+		printf("%d \n", s2[i]);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);	
+		i++;	
 	}
 	if (i > n)
-		return (-1);
-	return (s1[i] - s2[i]); 
+		return (s1[i] - s2[i]);
+	return (0);
 }
 
 int	main(void)
 {
 	printf("ft_strncmp %d\n", ft_strncmp("Hola","Hola ", 4));
+	printf("ft_strncmp %d\n", ft_strncmp("Hola","Hola" , 4));
+	printf("ft_strncmp %d\n", ft_strncmp("Hola","Hola", 3));
 	printf("ft_strncmp %d\n", ft_strncmp("Hola", "Holi", 4));
 	printf("ft_strncmp %d\n", ft_strncmp(" ", "\0", 5));
 	printf("ft_strncmp %d\n", ft_strncmp("hola", "hola", 5));
