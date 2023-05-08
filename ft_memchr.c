58f5e205546e9b4f7d63587c3f54954b7dc4f56f
Dionisia Martin Silva <dionmart@car5s5.42barcelona.com>
2023-05-08 13:05:58 +0200

    + ft_memset.c

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dionmart <dionmart@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 12:20:06 by dionmart          #+#    #+#             */
+/*   Updated: 2023/05/08 13:04:49 by dionmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*pt;
+	size_t			i;
+
+	pt = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (pt[i] == (unsigned char)c)
+		{
+			s += i;
+			return ((void *)s);
+		}
+		i++;
+	}
+	return (NULL);
+}
