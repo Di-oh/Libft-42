9fe6e2e959c27d330d2af84d3eacb4653f028589
Dionisia Martin Silva <dionmart@cbr7s1.42barcelona.com>
2023-05-11 18:11:56 +0200

    11/05/2023 18:11

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,12 @@
 /*   By: dionmart <dionmart@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:24:49 by dionmart          #+#    #+#             */
-/*   Updated: 2023/05/10 15:54:29 by dionmart         ###   ########.fr       */
+/*   Updated: 2023/05/11 18:11:16 by dionmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	i;
 	unsigned int	j;
@@ -24,16 +24,29 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 		return (haystrack);
 	while (haystack[j])
 	{
-		while (haystack[i] == needle[i] i < len)
+		j = 0;
+		x = i;
+		while (haystack[i] == needle[i] && haystrack[i] && i < len)
 		{
+			j++;
 			i++;
 		}
-		
+		if (little[j] == '\0')
+			big += x;
+		i++;
 	}
 	if (x == 0)
 		return (NULL)
 	else
-		 
 		return(needle);
+}
 
+int	main(void)
+{
+	char    a[13] = "Hola que tal?";
+	char	b[4] = "Hola";
+	
+	printf("",);
+
+	return (0);
 }
