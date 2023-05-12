edd3954b5b01ad61c30fc2db8f7617c5e947e0a1
Dio <dionisia.martin.silva@gmail.com>
2023-05-12 17:17:37 +0200

    12/05/2023 17:17

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,43 +11,38 @@
 /* ************************************************************************** */
 #include "libft.h"
 #include <stdio.h>
-/*
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	x;
 	char		*str;
-	char		*str2;
 	
-	str = haystack;
-	str2 = needle;
+	str = (char *)haystack;
 	i = 0;
 	j = 0;
 	x = 0;
 	if (ft_strlen(needle) == 0)
 		return (str);
-	while (haystack[j])
+	while (haystack[i] && j < (len - 1))
 	{		
 		j = 0;
-		x = i;
-		while (haystack[i] == needle[i] && haystack[i] && x >= len)
+		while (haystack[i] == needle[j] && haystack[i] && j < (len-1))
 		{
+			if (j == 0)
+				x = i;
 			j++;
 			i++;
-			x++;
 		}
 		i++;
 	}
-	if (x < i)
+	if (j > (len -1))
 	{
 		str += x;
        		return (str);	
-	}
-	if (x == 0)
+	}else
 		return (NULL);
-	else
-		return (str2);
 }
 
 int	main(void)
@@ -60,4 +55,60 @@ int	main(void)
 	printf("%s \n", a);
 
 	return (0);
-}*/
+}
+#include <string.h>
+
+int     ft_strncmp(const char *s1, const char *s2, int n)
+{
+        unsigned int    i;
+
+        i = 0;
+        while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+        {
+                if (s1[i] != s2[i])
+                        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+                i++;
+        }
+        if (i < n)
+                return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        return (0);
+}
+
+char    *ft_strnstr(const char *haystack, const char *needle, int len)
+{
+        unsigned int    i;
+        unsigned int    j;
+        unsigned int    x;
+        char            *str;
+
+        str = (char *)haystack;
+        i = 0;
+        j = 0;
+        x = 0;
+        if (!needle[i])
+                return (str);
+        while (haystack[i])
+        {
+                j = 0;
+                x = i;
+                while (haystack[i] == needle[j] && haystack[i] && j < (len-1))
+                {
+                  if (ft_strncmp(haystack[i], needle[j], len) == 0)
+                      return (str = str + x);
+                  j++;
+                  i++;
+                }
+                i++;
+        }
+}
+
+int     main(void)
+{
+        char    a[14] = "que quee tal?";
+        char    b[4] = "quee";
+
+        printf("%s \n", a);
+        printf("%s \n", ft_strnstr(a, b, 4));
+        printf("%s \n", a);
+        return (0);
+}
