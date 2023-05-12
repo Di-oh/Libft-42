015444854ff48e93af25ff8fa4d586bf4e892940
Dio <dionisia.martin.silva@gmail.com>
2023-05-12 13:14:45 +0200

    12/05/2023 13:14

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,35 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	i;
 	unsigned int	j;
-	char			*str;
+	unsigned int	x;
+	char		*str;
+	char		*str2;
 	
+	str = haystack;
+	str2 = needle;
 	i = 0;
 	j = 0;
-	x =	0;
+	x = 0;
 	if (ft_strlen(needle) == 0)
-		return (haystrack);
+		return (str);
 	while (haystack[j])
-	{
+	{		
 		j = 0;
 		x = i;
-		while (haystack[i] == needle[i] && haystrack[i] && i < len)
+		while (haystack[i] == needle[i] && haystack[i] && x >= len)
 		{
 			j++;
 			i++;
+			x++;
 		}
-		if (little[j] == '\0')
-			big += x;
 		i++;
 	}
+	if (x < i)
+	{
+		str += x;
+       		return (str);	
+	}
 	if (x == 0)
-		return (NULL)
+		return (NULL);
 	else
-		return(needle);
+		return (str2);
 }
 
 int	main(void)
@@ -46,7 +55,9 @@ int	main(void)
 	char    a[13] = "Hola que tal?";
 	char	b[4] = "Hola";
 	
-	printf("",);
+	printf("%s \n", a);
+	printf("%s \n", ft_strnstr(a, b, 4));
+	printf("%s \n", a);
 
 	return (0);
 }
