8cd50db7cbb7d55761f68dc7ee9cd5c0f0a9d8eb
Dio <dionisia.martin.silva@gmail.com>
2023-05-20 09:09:03 +0200

    20/05/2023 09:09

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -4,25 +4,37 @@
 char *ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
-	int	x;
-	int	y; 
 	int	i;
+	int	j;
+	int	z;
 
 	i = 0;
-	x = ft_strlen(s1);
-	y = ft_strlen(set);
-	str = malloc(sizeof(char) *(x - y + 1));
+	j = 0;
+	z = 0;
+	str = malloc(sizeof(char) *(ft_strlen(s1) - ft_strlen(set) + 1));
 	if (str == NULL)
 		return (NULL);
-	while (s1)
+	while (s1[i] != '\0')
 	{
-		while (i < z)
+		printf("valor 1 de s[i] : %c\n", s1[i]);
+	
+		j = 0;
+		while (s1[i] == set[j] && set[j] != '\0')
 		{
-			str[i] = s1[i];
+			printf("valor de s[i] : %c\n", s1[i]);
+			printf("valor de s[j] : %c\n", set[j]);
 			i++;
+			j++;
 		}
-		i  = i + y;
-		s1 += i;
+		str[z] = s1[i]; 
+		i++;
+		z++;
 	}
 	return (str);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_strtrim("lHola", "l"));
+	return (0);
 }
