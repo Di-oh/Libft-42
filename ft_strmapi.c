c6b05b3d3e8c4652484e1813e15aecd377c5f4e9
Dio <dionisia.martin.silva@gmail.com>
2023-05-25 08:53:25 +0200

    aproximacion strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,25 @@
+
+
+char	*f_new(unsigned int i, char a)
+{
+	char	*str;
+
+	str
+
+}
+
+char *ft_strmapi(char const *s, char *f_new(unsigned int i, char a ))
+{
+
+	unsigned int i;
+	char	*str;
+
+	i = 0;
+	while (s[i])
+	{
+		str = ft_new(i, s[i]);
+		i++;
+	}
+	return (str);
+}	
+
